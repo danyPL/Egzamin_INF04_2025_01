ae57b6eeb343cdad5d175db4d9a99b338a034ba2
danyPL <112093587+danyPL@users.noreply.github.com>
2025-04-08 14:12:50 +0200

    Add files via upload

---
+++ konsolowa/Program.cs
@@ -0,0 +1,55 @@
+﻿class Program
+{
+    /****************************************************
+   * nazwa funkcji: Algorytm
+   * typ zwracany: int, zwraca indeks równowagi
+   * informacje: Algorytm wyszukujacy indeks równowagi w tablicy
+   * autor: danyPL
+   **************************************************** 
+   */
+    static int Algorytm(List<int> nums)
+    {
+
+        int n = nums.Count;
+        int left, right;
+
+        for (int i = 0; i < n; i++)
+        {
+            left = 0;
+            for (int j = 0; j < i; j++)
+            {
+                left += nums[j];
+            }
+            right = 0;
+            for (int j = i + 1; j < n; j++)
+            {
+                right += nums[j];
+            }
+            if (left == right)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    /****************************************************
+     * nazwa funkcji: Main
+     * typ zwracany: Void, nic nie zwraca
+     * informacje: funckja inicjalizująca działanie aplikacji
+     * autor: danyPL
+     **************************************************** 
+     */
+    static void Main(string[] args)
+    {
+        string[] nums = Console.ReadLine().Split(",");
+        List<int> Cnums = new();
+        foreach(string num in nums)
+        {
+            Cnums.Add(Convert.ToInt32(num));
+        }
+        int wynik = Algorytm(Cnums);
+        Console.WriteLine($"Znaleziono indeks równowagi {wynik}");
+
+    }
+
+}
---
+++ mobilna/MainPage.xaml.cs
@@ -0,0 +1,94 @@
+﻿namespace INF_04_2025_01_Mobile
+{
+    public class Song
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Path { get; set; }
+        public string Time { get; set; }
+    }
+
+    public partial class MainPage : ContentPage
+    {
+        List<Song> Songs = new List<Song>();
+        Song CurrentSong;
+        int currentIndex = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            LoadMauiAsset();
+        }
+
+        public void Next(object sender, EventArgs e)
+        {
+            if (Songs.Count == 0) return;
+
+            currentIndex++;
+            if(currentIndex >= Songs.Count)
+            {
+                currentIndex = 0;
+            }
+            CurrentSong = Songs[currentIndex];
+            UpdateUI();
+        }
+
+        public void Back(object sender, EventArgs e)
+        {
+            if (Songs.Count == 0) return;
+
+            currentIndex--;
+            if(currentIndex < 0)
+            {
+                currentIndex=Songs.Count -1;
+            }
+            CurrentSong = Songs[currentIndex];
+            UpdateUI();
+        }
+
+        void UpdateUI()
+        {
+            Author.Text = CurrentSong.Author;
+            Title.Text = CurrentSong.Title;
+            Image.Source = CurrentSong.Path;
+            EndTime.Text = CurrentSong.Time;
+        }
+
+        async Task LoadMauiAsset()
+        {
+            try
+            {
+                using var stream = await FileSystem.OpenAppPackageFileAsync("music.txt");
+                using var reader = new StreamReader(stream);
+
+                var contents = await reader.ReadToEndAsync();
+                var lines = contents.Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var line in lines)
+                {
+                    var parts = line.Split(';');
+                    if (parts.Length >= 4)
+                    {
+                        Songs.Add(new Song
+                        {
+                            Author = parts[0],
+                            Title = parts[1],
+                            Time = parts[2],
+                            Path = parts[3]
+                        });
+                    }
+                }
+
+                if (Songs.Count > 0)
+                {
+                    CurrentSong = Songs[0];
+                    UpdateUI();
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", $"Błąd nie załadowano piosenek: {ex.Message}", "OK");
+            }
+        }
+    }
+}
